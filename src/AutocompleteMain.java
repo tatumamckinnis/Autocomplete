af28c979403d0a2a08e12ed26b9a6eab3d7b3cbb
Brandon Fain <brandon.fain@duke.edu>
2022-02-17 16:06:07 -0500

    removed driver

--- src/AutocompleteMain.java
+++ src/AutocompleteMain.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
@@ -14,21 +18,22 @@ public class AutocompleteMain {
 	final static String BRUTE_AUTOCOMPLETE = "BruteAutocomplete";
 	final static String BINARY_SEARCH_AUTOCOMPLETE = "BinarySearchAutocomplete";
 	final static String HASHLIST_AUTOCOMPLETE = "HashListAutocomplete";
-	
+
 	/* Modify name of Autocompletor implementation as necessary */
+
 	
 	final static String AUTOCOMPLETOR_CLASS_NAME = BRUTE_AUTOCOMPLETE;
 	//final static String AUTOCOMPLETOR_CLASS_NAME = BINARY_SEARCH_AUTOCOMPLETE;
 	//final static String AUTOCOMPLETOR_CLASS_NAME = HASHLIST_AUTOCOMPLETE;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		final int K = 10;
-		String filename = null;
+
 		File file = FileSelector.selectFile();
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new AutocompleteGUI(file.getAbsolutePath(),
-					 K, AUTOCOMPLETOR_CLASS_NAME).setVisible(true);
+					K, AUTOCOMPLETOR_CLASS_NAME).setVisible(true);
 			}
 		});
 	}
